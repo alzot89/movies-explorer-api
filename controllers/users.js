dece942e48a56c39f05bc33d244a9f84b736af1c
Sanches <sanches-zotov@yandex.ru>
2021-06-10 17:30:00 +0300

    fix: настроить куки

--- controllers/users.js
+++ controllers/users.js
@@ -45,8 +45,7 @@ const login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: 'none',
-          // secure: true,
+          path: '/',
         })
         .send({ message: 'вы успешно авторизовались' });
     })
